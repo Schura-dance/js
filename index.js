58f92d51a12dc2f3dfc9ba8afa42424323ea7913
Schura <rudenkovsasa@gmai.com>
2024-11-21 17:24:22 +0300

    Второе задание

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-// Начало проекта
 
-function sum(){
 
-    let sum = 0
-    for(let i = 0; i < arguments.length; i++){
-        sum += arguments[i]
+// Задание 2
+
+//Реализация_1
+function evenNumbers() {
+    const evenNumbersArray = [];
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] % 2 === 0) {
+            evenNumbersArray.push(arguments[i]);
+        }
     }
-    return sum;
+    return evenNumbersArray;
 }
+console.log(evenNumbers(0, 1, 2, 3, 4, 5));
 
-console.log(sum(1,2,3,4,5))
-console.log(sum())
-
-
-const sumNew = (...numbers) => numbers.reduce((sum, total) => sum + total, 0)
-console.log(sumNew(1,2,3))
-console.log(sumNew())
+//Реализация_2
+evenArr = (...numbers) => numbers.filter((num) => num % 2 === 0)
+console.log(evenArr(1, 5 , 8, 23, 5, 7, 8, 0))
